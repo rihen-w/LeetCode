38ff24ff83bd1b05cffc1c7ba032a801c32ef8a1
rihenkrishna <rihenww@gmail.com>
2025-08-12 20:29:09 +0530

    Time: 80 ms (8.39%), Space: 54.6 MB (61.7%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for( i = 0 ; i <= nums.length; i++){
+        for( j = 1+i ; j <= nums.length; j++ ){
+            if (nums[i]+nums[j]===target){
+                return [i,j]
+            }
+        }
+    }
+};
+console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([3,2,4], 6))
+console.log(twoSum([3,3], 6))
