a6a92b2a8ba140f12b17871b4924484a3ad21416
Rihen <rihenww@gmail.com>
2025-09-26 10:35:43 +0530

    Time: 0 ms (100%), Space: 54.1 MB (38.95%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    let lowercase = s.toLowerCase()
+    return lowercase
+};
+console.log(toLowerCase("Hello"))
+console.log(toLowerCase("here"))
+console.log(toLowerCase("LOVELY"))
