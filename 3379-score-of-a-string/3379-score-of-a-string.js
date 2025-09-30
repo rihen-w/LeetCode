e64f6657fb043f23c6bfaee61db51e7528732086
Rihen <rihenww@gmail.com>
2025-09-30 11:17:10 +0530

    Time: 0 ms (100%), Space: 54 MB (82.58%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var scoreOfString = function(s) {
+    let result = 0;
+
+    for(i = 0 ; i < s.length-1 ; i++){
+     result += Math.abs(s.charCodeAt(i) - s.charCodeAt(i+1))
+    }
+
+    return result
+};
